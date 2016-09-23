dd72642444b1e6829cafc458d43dabaac3fda8ef
Ben Zuba <zubes33@gmail.com>
2016-09-23 21:49:48 +0000

    update

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, :presence => {:message => "Come on bro, you need a title"}
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
-  end 
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
