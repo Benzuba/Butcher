95b0f77f0b0c44f18ef035976e3f063c7c70736f
Ben Zuba <zubes33@gmail.com>
2016-10-14 19:47:52 +0000

    set up student course page and course instructor add section

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,5 +1,18 @@
 class Instructor::SectionsController < ApplicationController
-  def new 
+  def new
     @course = Course.find(params[:course_id])
-  end 
+    @section = Section.new
+  end
+
+  def create
+    @course = Course.find(params[:course_id])
+    @section = @course.sections.create(section_params)
+    redirect_to instructor_course_path(@course)
+  end
+
+  private
+
+  def section_params
+    params.require(:section).permit(:title)
+  end
 end
