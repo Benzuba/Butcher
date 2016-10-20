a2ca74c78d28a3d96f4427cd0cc9390c187a8d73
Ben Zuba <zubes33@gmail.com>
2016-10-20 21:21:51 +0000

    update

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,9 @@
 class Course < ActiveRecord::Base
   belongs_to :user
-  has_many :sections
 
-  validates :title, :presence => {:message => "Come on bro, you need a title"}
+  validates :title, presence: true
   validates :description, presence: true
-  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :cost, presence: true
 
   mount_uploader :image, ImageUploader
 end
