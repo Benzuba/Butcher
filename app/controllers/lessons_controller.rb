56f06f2c6e6084c4826b151e9b300e6de1f450d9
Ben Zuba <zubes33@gmail.com>
2016-10-21 23:04:37 +0000

    switched to amazon S3 for uploads and payments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
-  end 
+  end
 
-
-  private 
+  private
+  
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-end 
+  
+  def require_currently_enrolled
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You are not enrolled"
+    end
+  end
+end
