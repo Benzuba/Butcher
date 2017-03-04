f4bc648ad6a0c06f638a471f6f037e495f8bd2aa
Ben Zuba <zubes33@gmail.com>
2017-03-04 20:17:44 +0000

    updated styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,74 +11,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_161_201_150_840) do
+ActiveRecord::Schema.define(version: 20161201150840) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'blahs', force: true do |t|
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "blahs", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'courses', force: true do |t|
-    t.string   'title'
-    t.text     'description'
-    t.decimal  'cost'
-    t.integer  'user_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string   'image'
+  create_table "courses", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "cost"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image"
   end
 
-  add_index 'courses', ['user_id'], name: 'index_courses_on_user_id', using: :btree
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
 
-  create_table 'enrollments', force: true do |t|
-    t.integer  'user_id'
-    t.integer  'course_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "enrollments", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index 'enrollments', ['course_id'], name: 'index_enrollments_on_course_id', using: :btree
-  add_index 'enrollments', %w(user_id course_id), name: 'index_enrollments_on_user_id_and_course_id', using: :btree
+  add_index "enrollments", ["course_id"], name: "index_enrollments_on_course_id", using: :btree
+  add_index "enrollments", ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id", using: :btree
 
-  create_table 'lessons', force: true do |t|
-    t.string   'title'
-    t.string   'subtitle'
-    t.integer  'section_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'row_order'
+  create_table "lessons", force: true do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.integer  "section_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
-  add_index 'lessons', ['row_order'], name: 'index_lessons_on_row_order', using: :btree
-  add_index 'lessons', ['section_id'], name: 'index_lessons_on_section_id', using: :btree
+  add_index "lessons", ["row_order"], name: "index_lessons_on_row_order", using: :btree
+  add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
 
-  create_table 'sections', force: true do |t|
-    t.string   'title'
-    t.integer  'course_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'row_order'
+  create_table "sections", force: true do |t|
+    t.string   "title"
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
-  add_index 'sections', ['course_id'], name: 'index_sections_on_course_id', using: :btree
+  add_index "sections", ["course_id"], name: "index_sections_on_course_id", using: :btree
 
-  create_table 'users', force: true do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.inet     'current_sign_in_ip'
-    t.inet     'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', unique: true, using: :btree
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
